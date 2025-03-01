3e787f468059d57bb7a2fe8249f99d544957aa58
Eric Glomstad <eric@crazyerics.com>
2025-02-28 17:43:17 -0800

    zoom and pan fixes

--- client/src/app/components/image-gallery/image-gallery.component.ts
+++ client/src/app/components/image-gallery/image-gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, HostListener, EventEmitter, Output } from '@angular/core';
+import { Component, Input, OnInit, HostListener, EventEmitter, Output, ElementRef, ViewChild } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { IImage } from 'src/models/image';
 
@@ -8,153 +8,77 @@ import { IImage } from 'src/models/image';
     styleUrls: ['./image-gallery.component.scss']
 })
 export class ImageGalleryComponent implements OnInit {
-
     @Input() path!: string;
     @Input() images!: IImage[];
     @Input() set showImage(image: IImage) {
-        
         this.currentImageIndex = this.images.findIndex((item: IImage) => item.name === image.name);
         this.setGalleryImage();
     }
     @Output() closeGalleryRequest = new EventEmitter<void>();
-    
-    @HostListener('window:keydown.q', ['$event'])
-    KeyDownKeyQ(event: KeyboardEvent) {
-        this.closeGallery();
-    }
-    
-    @HostListener('window:keydown.arrowleft', ['$event'])
-    KeyDownArrowLeft(event: KeyboardEvent) {
-        this.previousImage();
-        event.stopPropagation();
-    }
-    
-    @HostListener('window:keydown.arrowright', ['$event'])
-    KeyDownArrowRight(event: KeyboardEvent) {
-        this.nextImage();
-        event.stopPropagation();
-    }
 
-    @HostListener('window:keydown.arrowup', ['$event'])
-    KeyDownArrowUp(event: KeyboardEvent) {
-        this.showControls();
-        this.slideShowIntervalInMilliseconds -= 500;
-        if (this.slideShowActive) {
-            this.stopSlideShow();
-            this.startSlideShow();
-        }
-        event.stopPropagation();
-    }
-    
-    @HostListener('window:keydown.arrowdown', ['$event'])
-    KeyDownArrowDown(event: KeyboardEvent) {
-        this.showControls();
-        this.slideShowIntervalInMilliseconds += 500;
-        if (this.slideShowActive) {
-            this.stopSlideShow();
-            this.startSlideShow();
-        }
-        event.stopPropagation();
-    }
+    @ViewChild('imageContainer', { static: false }) imageContainer!: ElementRef;
 
-    @HostListener('window:keydown.space', ['$event'])
-    Space(event: KeyboardEvent) {
-        this.toggleSlideShow();
-        event.stopPropagation();
-    }
+    @HostListener('window:keydown.q', ['$event']) KeyDownKeyQ(event: KeyboardEvent) { this.closeGallery(); }
+    @HostListener('window:keydown.arrowleft', ['$event']) KeyDownArrowLeft(event: KeyboardEvent) { this.previousImage(); event.stopPropagation(); }
+    @HostListener('window:keydown.arrowright', ['$event']) KeyDownArrowRight(event: KeyboardEvent) { this.nextImage(); event.stopPropagation(); }
+    @HostListener('window:keydown.space', ['$event']) Space(event: KeyboardEvent) { this.toggleSlideShow(); event.stopPropagation(); }
 
-    @HostListener('mousewheel', ['$event'])
-    Mousewheel(event: WheelEvent) {
+    @HostListener('wheel', ['$event']) onMouseWheel(event: WheelEvent) {
         event.preventDefault();
-        this.imageScale += (event.deltaY > 0) ? -0.1 : 0.1;
-        // console.log(this.imageScale);
+        this.adjustZoom(event.deltaY);
     }
 
-    @HostListener('mousedown', ['$event'])
-    Mousedown(event: MouseEvent) {
-        this.mouseMovementX = 0;
-        this.mouseMovementY = 0;
-        this.isMouseDown = true;
+    @HostListener('mousedown', ['$event']) onMouseDown(event: MouseEvent) {
+        this.isDragging = true;
+        this.lastMouseX = event.clientX;
+        this.lastMouseY = event.clientY;
     }
 
-    @HostListener('mouseup', ['$event'])
-    Mouseup(event: MouseEvent) {
-        this.isMouseDown = false;
-    }
+    @HostListener('mouseup') onMouseUp() { this.isDragging = false; }
 
-    @HostListener('mousemove', ['$event'])
-    Mousemove(event: MouseEvent) {
-        
-        this.showControls();
-        
-        if (this.isMouseDown) {
+    @HostListener('mousemove', ['$event']) onMouseMove(event: MouseEvent) {
+        if (!this.isDragging) return;
 
-            const x = event.screenX;
-            const y = event.screenY;
+        const deltaX = event.clientX - this.lastMouseX;
+        const deltaY = event.clientY - this.lastMouseY;
 
-            if (this.mouseMovementX === 0 && this.mouseMovementY === 0) {
-                this.mouseMovementX = x;
-                this.mouseMovementY = y;
-                return;
-            }
+        this.imagePositionX += deltaX / this.imageScale;  // Adjust based on zoom level
+        this.imagePositionY += deltaY / this.imageScale;
 
-            // lateral movement
-            if (x > this.mouseMovementX) { // mouse moved right
-                this.imagePositionPercentageX += this.imageScale;
-            }
-            else if (x < this.mouseMovementX) { // mouse moved left
-                this.imagePositionPercentageX -= this.imageScale;
-            }
-
-            // long movement
-            if (y > this.mouseMovementY) { // mouse moved down
-                this.imagePositionPercentageY += this.imageScale;
-            }
-            else if (y < this.mouseMovementY) { //mouse moved up
-                this.imagePositionPercentageY -= this.imageScale;
-            }
-
-            this.mouseMovementX = x;
-            this.mouseMovementY = y;
-        }
+        this.lastMouseX = event.clientX;
+        this.lastMouseY = event.clientY;
     }
 
     public imageScale = 1;
+    public minZoom = 0.5;
+    public maxZoom = 3;
     public currentImageIndex = 0;
-    public imageUrl!: string;  
-    public imagePositionPercentageX = 50;
-    public imagePositionPercentageY = 50;
+    public imageUrl!: string;
+    public imagePositionX = 0;
+    public imagePositionY = 0;
     public slideShowActive = false;
     public slideShowIntervalInMilliseconds = 3000;
     public hideControls = false;
     public showImageLegend = environment.application.showImageLegend;
 
-    private isMouseDown = false;
-    private mouseMovementX = 0;
-    private mouseMovementY = 0;
+    private isDragging = false;
+    private lastMouseX = 0;
+    private lastMouseY = 0;
     private slideShowInterval!: ReturnType<typeof setInterval>;
     private countdownTimerToHideControls!: ReturnType<typeof setTimeout>;
     private countdownToHideControlsInMilliseconds = 3000;
 
     constructor() { }
 
-    ngOnInit(): void {
-
-    }
+    ngOnInit(): void { }
 
     previousImage(): void {
-        this.currentImageIndex--;
-        if (this.currentImageIndex < 0) {
-            this.currentImageIndex = this.images.length -1;
-        }
+        this.currentImageIndex = (this.currentImageIndex - 1 + this.images.length) % this.images.length;
         this.setGalleryImage();
     }
 
     nextImage(): void {
-        this.currentImageIndex++;
-        if (this.currentImageIndex >= this.images.length) {
-            this.currentImageIndex = 0;
-        }
+        this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
         this.setGalleryImage();
     }
 
@@ -166,17 +90,13 @@ export class ImageGalleryComponent implements OnInit {
 
     setGalleryImage(): void {
         this.imageScale = 1;
-        this.imagePositionPercentageX = 50;
-        this.imagePositionPercentageY = 50;
+        this.imagePositionX = 0;
+        this.imagePositionY = 0;
         this.imageUrl = environment.apis.httpServer + '/' + this.images[this.currentImageIndex].url;
     }
 
     toggleSlideShow(): void {
-        if (this.slideShowActive) {
-            this.stopSlideShow();
-        } else {
-            this.startSlideShow();
-        }
+        this.slideShowActive ? this.stopSlideShow() : this.startSlideShow();
     }
 
     stopSlideShow(): void {
@@ -186,26 +106,15 @@ export class ImageGalleryComponent implements OnInit {
 
     startSlideShow(): void {
         this.slideShowActive = true;
-        this.slideShowInterval = setInterval(() => {
-            this.nextImage();
-        }, this.slideShowIntervalInMilliseconds);
+        this.slideShowInterval = setInterval(() => { this.nextImage(); }, this.slideShowIntervalInMilliseconds);
     }
 
-    showControls(): void {
-        clearTimeout(this.countdownTimerToHideControls);
-        this.hideControls = false;
-        this.countdownTimerToHideControls = setTimeout(() => {
-            this.hideControls = true;
-        }, this.countdownToHideControlsInMilliseconds);
-    }
-
-    navigationLeftClicked(): void {
-        this.showControls();
-        this.previousImage();
-    }
-
-    navigationRightClicked(): void {
-        this.showControls();
-        this.nextImage();
+    adjustZoom(delta: number): void {
+        const zoomFactor = delta > 0 ? -0.1 : 0.1;
+        const newScale = this.imageScale + zoomFactor;
+        
+        if (newScale >= this.minZoom && newScale <= this.maxZoom) {
+            this.imageScale = newScale;
+        }
     }
 }
